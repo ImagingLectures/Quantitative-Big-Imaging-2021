802acd494dbe9def37cea950b0f4762416ffc6f7
Anders Kaestner <anders.kaestner@psi.ch>
2021-04-06 21:11:41 +0200

    The book builds locally.

---
+++ building/buildLecture07.sh
@@ -0,0 +1,6 @@
+unzip Lectures/Lecture-07/data/grains.npy.zip
+unzip Lectures/Lecture-07/data/ws_grains.npy.zip
+
+jupyter-book build Lectures/Lecture-07 --builder pdflatex
+
+cp Lectures/Lecture-07/_build/latex/QBI-Lecture07-ComplexShape.pdf docs/
